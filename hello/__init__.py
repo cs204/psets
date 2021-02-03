18ac41aa640da5cb19a7998f1b3fb7e2532e86b6
vadimgb <vadimgb@yandex.ru>
2021-02-03 21:58:30 +0700

    work

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,6 +1,12 @@
 import check50
+import check50.c
 
 @check50.check()
-def hello_world():
-    """hello world"""
-    check50.run("node hello.js").stdout("Hello, world!", regex=False).exit(0)
+def exists():
+    """hello.js существует"""
+    check50.exists("hello.js")
+
+@check50.check(exists)
+def hello():
+    """печатает Hello, world"""
+    check50.run("node hello.js").stdout("Hello, world!").exit()
